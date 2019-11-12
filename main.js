9802e51a7023c573542f4b8fd7c6daeb4222ef63
David Balderston <dbalders@gmail.com>
2019-11-11 16:16:03 -0800

    app calls api on startup now
    still need to setup auto launch, it to call the api each day, and get each team's record

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const {app, BrowserWindow, ipcMain, Tray} = require('electron');
+const { app, BrowserWindow, ipcMain, Tray } = require('electron');
+const fetch = require('electron-fetch')
 const path = require('path');
+const fs = require('fs')
+
+//setup auto launch
+//setup scheduler
+//Add team records
 
 let tray = undefined
 let window = undefined
@@ -8,67 +14,120 @@ let window = undefined
 app.dock.hide()
 
 app.on('ready', () => {
-  createTray()
-  createWindow()
+    nbaAPI()
 })
 
 const createTray = () => {
-  tray = new Tray(path.join('basketball.png'))
-  tray.on('click', function (event) {
-    toggleWindow()
-  });
+    tray = new Tray(path.join('basketball.png'))
+    tray.on('click', function (event) {
+        toggleWindow()
+    });
 }
 
 const getWindowPosition = () => {
-  const windowBounds = window.getBounds();
-  const trayBounds = tray.getBounds();
+    const windowBounds = window.getBounds();
+    const trayBounds = tray.getBounds();
 
-  // Center window horizontally below the tray icon
-  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
+    // Center window horizontally below the tray icon
+    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
 
-  // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 4);
+    // Position window 4 pixels vertically below the tray icon
+    const y = Math.round(trayBounds.y + trayBounds.height + 4);
 
-  return {x: x, y: y};
+    return { x: x, y: y };
 }
 
 const createWindow = () => {
-  window = new BrowserWindow({
-    width: 200,
-    height: 450,
-    show: false,
-    frame: false,
-    fullscreenable: false,
-    resizable: true,
-    transparent: false,
-    webPreferences: {
-      backgroundThrottling: false,
-      nodeIntegration: true
-    }
-  })
-//   window.webContents.openDevTools()
-  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+    window = new BrowserWindow({
+        width: 200,
+        height: 450,
+        show: false,
+        frame: false,
+        fullscreenable: false,
+        resizable: true,
+        transparent: false,
+        webPreferences: {
+            backgroundThrottling: false,
+            nodeIntegration: true
+        }
+    })
+    //   window.webContents.openDevTools()
+    window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
-  // Hide the window when it loses focus
-  window.on('blur', () => {
-    if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
-    }
-  })
+    // Hide the window when it loses focus
+    window.on('blur', () => {
+        if (!window.webContents.isDevToolsOpened()) {
+            window.hide()
+        }
+    })
 }
 
 //Need to add in some sort of api call to update the nba.json all the time
 
+const nbaAPI = () => {
+    var date = new Date()
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = date.getFullYear();
+    var fullDate = mm + '/' + dd + '/' + yyyy;
+    var url = 'https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=' + fullDate;
+
+    const https = require('https');
+
+    https.get(url, (resp) => {
+      let data = '';
+    
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+    
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        console.log(JSON.parse(data));
+        fs.writeFileSync('nba.json', data);
+        console.log(app.isReady())
+        createTray()
+        createWindow()
+      });
+    
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+
+    // var request = require('request');
+    // request(url, function (error, response, body) {
+    //     console.log(error)
+    //     if (!error && response.statusCode == 200) {
+    //         console.log(body) // Print the google web page.
+    //     }
+    // })
+
+    // fetch('https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=' + fullDate)
+    //     .then(response => {
+    //         console.log(response)
+    //         return response.json()
+    //     })
+    //     .then(data => {
+    //         // Work with JSON data here
+    //         console.log(data)
+    //     })
+    //     .catch(err => {
+    //         // Do something for an error here
+    //         console.log(err)
+    //     })
+}
+
 const toggleWindow = () => {
-  window.isVisible() ? window.hide() : showWindow();
+    window.isVisible() ? window.hide() : showWindow();
 }
 
 const showWindow = () => {
-  const position = getWindowPosition();
-  window.setPosition(position.x, position.y, false);
-  window.show();
+    const position = getWindowPosition();
+    window.setPosition(position.x, position.y, false);
+    window.show();
 }
 
 ipcMain.on('show-window', () => {
-  showWindow()
+    showWindow()
 })
